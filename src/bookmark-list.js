dab9d32854aa5f04c3ff353d1f7381f4673e89a5
JaKel Jenkins <jakelj24@gmail.com>
2020-10-04 14:53:21 -0400

    Update

--- src/bookmark-list.js
+++ src/bookmark-list.js
@@ -5,60 +5,58 @@ import api from './api';
 
 const handleItemClicked = function () {
     $('.boxed').on('click', '.bookmark-item', event => {
-        const item = event.currentTarget;
-        const id = getItemIdFromElement(event.currentTarget);
-        console.log(item)
-        console.log(id);
-        store.findAndToggleExpanded(id);
-        render();
+        const item = event.currentTarget
+        const id = getItemIdFromElement(event.currentTarget)
+        store.findAndToggleExpanded(id)
+        render()
     });
 }
 const getItemIdFromBtn = function (item) {
     return $(item)
         .closest('.delete-item')
-        .data('item-id');
+        .data('item-id')
 };
 const getItemIdFromElement = function (item) {
     return $(item)
         .closest('.bookmark-item')
-        .data('item-id');
+        .data('item-id')
 };
 
 const newItemSubmit = function () {
     $('#submit').on('click', function (event) {
-        event.preventDefault();
-        store.state += 1;
-        render();
+        event.preventDefault()
+        store.state += 1
+        render()
     })
 }
 
 const createNewItem = function () {
     $('#createBtn').on('click', function (event) {
-        event.preventDefault();
-        const title = $('#bookmark-title').val();
-        const rating = $(`input[name = "radio"]:checked`).val();
-        const url = $('#bookmark-url').val();
-        const description = $('#description').val();
+        event.preventDefault()
+        const title = $('#bookmark-title').val()
+        const rating = $(`input[name = "radio"]:checked`).val()
+        const url = $('#bookmark-url').val()
+        const description = $('#description').val()
         const expanded = false;
-        if(title != null && description != null && rating > 0 && url.includes("https://")){
-        api.createItem(title, url, description, rating)
-            .then(res => res.json())
-            .then((newItem) => {
-                store.addItem(newItem);
-                store.state -= 1;
-                render();
-            })
-        }else{
+        if (title != '' && description != '' && rating > 0 && url.includes("https://")) {
+            api.createItem(title, url, description, rating)
+                .then(res => res.json())
+                .then((newItem) => {
+                    store.addItem(newItem)
+                    store.state -= 1;
+                    render()
+                })
+        } else {
             //Self generated error
-            store.setError("Title, Url (with https://) , Rating & Description Required!");
-            renderError();
+            store.setError("Title, Url (with https://) , Rating & Description Required!")
+            renderError()
         }
-            // .catch((error) => {
-            //     store.setError(error.message);
-            //     renderError();
-            // });
-    });
-};
+        // .catch((error) => {
+        //     store.setError(error.message);
+        //     renderError();
+        // });
+    })
+}
 
 
 const deleteItem = function () {
@@ -66,7 +64,6 @@ const deleteItem = function () {
         event.preventDefault();
         const item = event.currentTarget;
         const id = getItemIdFromBtn(event.currentTarget);
-        console.log(id);
         api.deleteItem(id)
             .then(res => res.json())
             .then((newItem) => {
@@ -89,8 +86,8 @@ const generateError = function (message) {
           <button id="cancel-error">X</button>
           <p>${message}</p>
         </section>
-      `;
-};
+      `
+}
 
 const renderError = function () {
     if (store.error) {
@@ -99,28 +96,22 @@ const renderError = function () {
     } else {
         $('.error-container').empty();
     }
-};
+}
 
 const handleCloseError = function () {
     $('.error-container').on('click', '#cancel-error', () => {
         store.setError(null);
         renderError();
-    });
-};
-
-//$('#stars').addEventListener("change", store.filter = option.value);
-
-// $('#stars').live('change', function(e) {
-//     console.log(e.target.options[e.target.selectedIndex].text);
-// });
+    })
+}
 
 const handleDropDown = function () {
-    $('#stars').on('click', function (v) {
-        console.log(v.target.options[v.target.selectedIndex].text);
-        v.preventDefault();
-        v.stopPropagation();
-        store.filter = v.currentTarget.value;
-        render();
+    $('form').on('change', 'select', function () {
+        let rating = $('select').val();
+        if (rating) {
+            store.filter = rating;
+            render();
+        }
     })
 }
 
@@ -131,30 +122,29 @@ const generateBookmarkItemsString = function (bookmarkList) {
 };
 
 const generateBookmarks = function (item) {
-    console.log(item)
     //This is the actual creation of the bookmark with its star ratings and info
-    let rating = item.rating;
-    let descItem = "";
-    let itemTitle = '';
-    let starRating1 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`;
+    let rating = item.rating
+    let descItem = ""
+    let itemTitle = ''
+    let starRating1 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`
     rating -= 1;
-    let starRating2 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`;
+    let starRating2 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`
     rating -= 1;
-    let starRating3 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`;
+    let starRating3 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`
     rating -= 1;
-    let starRating4 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`;
+    let starRating4 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`
     rating -= 1;
-    let starRating5 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`;
+    let starRating5 = `<i class = "fa fa-star ${rating > 0 ? 'fa fa-star checked' : 'fa fa-star'}"></i>`
     rating -= 1;
 
     if (item.expanded) {
         descItem = `<form action = "${item.url}">
         <button class = "visit">Visit Site</button></br>` + "  " + item.description +
             `</br><button class = "delete-item"  data-item-id = "${item.id}">Delete Bookmark</button>
-            </form>`;
+            </form>`
     }
 
-    itemTitle = `<span class="bookmark-item" data-item-id="${item.id}">${item.title}
+    itemTitle = `<span tabindex='0' class="bookmark-item" data-item-id="${item.id}">${item.title}
         ${starRating1}
         ${starRating2}
         ${starRating3}
@@ -164,29 +154,28 @@ const generateBookmarks = function (item) {
     <div class ="discriptor">
         ${descItem}
     </div>`;
-    if (store.filter > 0 && item.rating != store.filter) {
+    if (store.filter > 0 && item.rating < store.filter) {
         itemTitle = '';
     }
     return `
      ${itemTitle}
 </div>  
-</div>`;
+</div>`
 }
 
 
 const startTemplate = function () {
-    return `<div class="container">
-<h1>My BookMarks</h1>
+    return `
 <form id="js-bookmark-form">
     <button type="submit" id = "submit">New</button>
-    <select id = "stars">
-        <option value="" disabled selected hidden>Filter</option>
-        <option value="1">1 Star</option>
-        <option value="2">2 Stars</option>
-        <option value="3">3 Stars</option>
-        <option value="4">4 Stars</option>
-        <option value="5">5 Stars</option>
-    </select>
+    <select name="rating" id="rating">
+                    <option value="">Filter By</option>
+                    <option value="1">(1 Star) &#9733;</option>
+                    <option value="2">(2 Stars) &#9733; &#9733;</option>
+                    <option value="3">(3 Stars) &#9733; &#9733; &#9733;</option>
+                    <option value="4">(4 Stars) &#9733; &#9733; &#9733; &#9733;</option>
+                    <option value="5">(5 Stars) &#9733; &#9733; &#9733; &#9733; &#9733;</option> 
+                </select>
 </form>
 <div class="boxed"></div>`
 }
@@ -196,22 +185,23 @@ const newItemTemplate = function () {
     <h1>Add New BookMark</h1>
     <form id="js-bookmark-submit-form">
         <label for = "bookmark-url" ></label>
-        <input required id = "bookmark-url" type = "text" name = "bookmark-url" placeholder = "https://url here">
+        <input id = "bookmark-url" type = "text" name = "bookmark-url" placeholder = "https://url here" required>
         <label for = "bookmark-title" ></label>
         <div></div>
-        <input required id = "bookmark-title" type = "text" name = "bookmark-title" placeholder = "Title Here">
+        <input  id = "bookmark-title" type = "text" name = "bookmark-title" placeholder = "Title Here" required>
         <div></div>
         <h2>Rate This Item</h2>
-        <input type="radio" id = "ratings" name="radio" value="1">
-        <label for = "1 star">1</label>
-        <input type="radio" id = "ratings" name="radio" value="2">
-        <label for = "2 star">2</label>
-        <input type="radio" id = "ratings" name="radio" value="3">
-        <label for = "3 star">3</label>
-        <input type="radio" id = "ratings" name="radio" value="4">
-        <label for = "4 star">4</label>
-        <input type="radio" id = "ratings" name="radio" value="5">
-        <label for = "5 star">5</label>
+        <label for = "1-star">1</label>
+        <input type="radio" id = "1-star" name="rating" value="1">
+        <label for = "2-star">2</label>
+        <input type="radio" id = "2-star" name="rating" value="2">
+        <label for = "3-star">3</label>
+        <input type="radio" id = "3-star" name="rating" value="3">
+        <label for = "4-star">4</label>
+        <input type="radio" id = "4-star" name="rating" value="4">
+        <label for = "5-star">5</label>
+        <input type="radio" id = "5-star" name="rating" value="5">
+        
         <div></div>
     
         <textarea required id = "description" name = "description" placeholder="URL Description Here..."></textarea>
